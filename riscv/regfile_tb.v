d73d43068bb407b5975dff06010e9170ac41049a
Kanaya Ozora <kanaya.ozora@tum.de>
2025-12-05 13:26:16 +0100

    reg file testbench

--- riscv/regfile_tb.v
+++ riscv/regfile_tb.v
@@ -13,8 +13,8 @@ module regfile_tb();
     reg we3; //write enable bit
 
     //Outputs
-    reg [31:0] rd1;
-    reg [31:0] rd2;
+    wire [31:0] rd1;
+    wire [31:0] rd2;
 
     //Instantiation
     register_file dut(
@@ -24,6 +24,7 @@ module regfile_tb();
         .a2(a2),
         .a3(a3),
         .wd3(wd3),
+        .we3(we3),
         .rd1(rd1),
         .rd2(rd2)
     );
@@ -36,21 +37,67 @@ module regfile_tb();
     //Waveform dump
     initial begin
         $dumpfile("regfile_dump.vcd");
-        $dumpvars(0, pc_tb);
+        $dumpvars(0, regfile_tb);
     end
 
     //Main test
     initial begin
         //initialize input
-        reset_n = 1; a1, a2, a3 = 0;
+        reset_n = 1; a1 = 0; a2 = 0; a3 = 0; we3 = 0;
 
         @(posedge clk);
         @(posedge clk);
 
         reset_n = 0;
 
-        //Test 1:
-        //Test 2:
+        //Test 1: Write to a register ADDI x%, x0, 10
+        $display("Test 1: Write data. Expected - Registers[5] = 10");
+        a3 =5; 
+        wd3 = 10;
+        we3 = 1;
+        @(posedge clk);
+        we3 = 0;
+
+        //Test 2: Write to another register ADDI x6, x0, 42
+        $display("Test 2: Write to another reg. Expected - Registers[6] = 42");
+        a3 = 6;
+        wd3 = 42;
+        we3 = 1;
+        @(posedge clk);
+
+        //Test 3: Read 2 registers ADD x7, x5, x6
+        $display("Test 3: Read two regs");
+        we3 = 0;
+        a1 = 5; 
+        a2 = 6; 
+        @(posedge clk);
+
+        a3 = 7;
+        we3 = 1;
+        wd3 = rd1 + rd2;
+        @(posedge clk);
+        we3 = 0;
+
+        //Test 4: write to x0 -- should not change x0
+        $display("Test 4: Write to x0");
+        a3 = 0; 
+        wd3 = 123; 
+        we3 = 1;
+        @(posedge clk);
+        we3 = 0;
+
+        //Test 5: Reset reg file
+        $display("Test 5: Reset reg file");
+        reset_n = 1; 
+        @(posedge clk);
+        reset_n = 0; 
+
+        $finish;
+    end
+
+    initial begin
+        $monitor("Time=%0t | rd1=%d | rd2=%d | a3=%d | wd3=%d | write enable=%b", 
+         $time, rd1, rd2, a3, wd3, we3);
     end
 
 endmodule
