efc8cd728abea20f5136605e7aa9d94ccb3f032e
Kanaya Ozora <kanaya.ozora@tum.de>
2025-12-05 21:06:36 +0100

    Fix sign extender

--- riscv/signextender.v
+++ riscv/signextender.v
@@ -1,15 +1,21 @@
 module signextender (
     input [24:0] A, 
-    input [1:0] sel_ext,
+    input [2:0] sel_ext,
     output reg[31:0] out
 );
 
 always @(*) begin //TODO: Tambah B, J, L stlh testbench
     case (sel_ext) 
-    2'b00, 2'b10: //lw, I-type
+    3'b000, 3'b010: //lw, I-type
         out = {{20{A[24]}}, A[24:13]};
-    2'b01: //sw
+    3'b001: //sw
         out = {{20{A[24]}}, A[24:18], A[4:0]};
+     3'b011: // B-type (beq)
+        out = {{19{A[24]}}, A[24], A[0], A[23:18], A[4:1], 1'b0};
+    3'b100: // J-type (jal)
+        out = {{11{A[24]}}, A[24], A[12:5], A[13], A[23:14], 1'b0};
+    3'b101: // U-type (lui)
+        out = {A[24:5], 12'b0};
     default: out = 32'b0; 
     endcase
 end
